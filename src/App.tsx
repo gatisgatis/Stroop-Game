039b3da1a14776d7a6f0ff4cc96f8e1b13fbe9de
Gatis <gatis.meikulis@gmail.com>
2020-11-18 14:34:05 +0200

    initial template push

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { useState, useRef, useEffect } from 'react';
+import './App.css';
+
+
+const App = () => {
+ 
+  return (
+    <div className="App">
+      <h1>This is an App</h1>
+    </div>
+  );
+};
+
+export default App;
